c41d53992a54678a5cf34cd307c85f6bbba36ba7
BT_1N3_16 <fwilking4736@bths.edu>
2019-01-17 08:40:16 -0500

    stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        double[] scan = random(12);
+        double[] scan2 = random(6);
 
+        SkyView sv1 = new SkyView(4, 3, scan);
+        SkyView sv2 = new SkyView(3, 2, scan2);
+
+        sv1.print();
+        sv2.print();
+
+        System.out.println("Average 1: " + sv1.getAverage(1, 3, 0, 2));
+        System.out.println("Average 2: " + sv2.getAverage(0, 1, 0, 1));
+    }
+
+    public static double[] random(int len){
+        double[] output = new double[len];
+
+        for(int i = 0; i < len; i++){
+            output[i] = Math.random() * 1.6;
+        }
+
+        return output;
     }
 }
--- src/com/company/SkyView.java
+++ src/com/company/SkyView.java
@@ -4,15 +4,47 @@ public class SkyView {
 
     private double[][] view;
 
-    public SkyView(int numRows, int numCols, double[] scanned){
-        this.view = new double[numRows][numCols];
+    public SkyView(int numCols, int numRows, double[] scanned){
+
+        view = new double[numCols][numRows];
+
+        numCols--;
+        numRows--;
         for(int i = 0; i < scanned.length; i++){
             if((i / numCols) % 2 == 0) {
-                this.view[i / numCols][i % numRows] = scanned[i];
+                view[i / numCols][i % (numRows + 1)] = scanned[i];
             }
             else{
-                this.view[i / numCols][numRows - i % numRows] = scanned[i];
+                view[i / numCols][numRows - i % (numRows + 1)] = scanned[i];
             }
         }
+    }
+
+    public void print(){
+        for(int i = 0; i < view.length; i++){
+            for(int j = 0; j < view[i].length; j++){
+                if(i % 2 == 0) {
+                    System.out.println(i + " " + j);
+                    System.out.print(view[i][j] + " ");
+                }
+                else{
+                    System.out.println(i + " " + (view.length - j));
+                    System.out.print(view[i][view.length - j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public double getAverage(int startRow, int endRow, int startCol, int endCol){
+        double total = 0;
+
+        for(int i = startRow; i < endRow; i++){
+            for(int j = startCol; i < endCol; j++){
+                total += view[i][j];
+            }
+        }
+
+        return total / ((startRow - endRow) * (startCol - endCol));
     }
 }
